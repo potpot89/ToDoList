c0ae62aa5a95d04489d15a5c13c3f2a2a97bb1a6
Jamila <135280536+potpot89@users.noreply.github.com>
2024-01-07 17:55:15 +0100

    added #TODO instructions

---
+++ script.js
@@ -0,0 +1,11 @@
+//create variables that select the HTML elements we need to use for interactivity
+
+const addButton = document.querySelector(`add-button`);
+const inputField = document.querySelector(`#list-input`);
+let shoppingList = document.querySelector(`#shopping-list`);
+
+// add event listener
+//add event listener to the button
+// on click create the <li> element in HTML
+// for each <li> add a button to mark it as done
+//after marking as done, remove it from the list
